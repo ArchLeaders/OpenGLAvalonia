a12d4112cd5054d97527936ce48bfa90e8b8c5a9
ArchLeaders <archleadership28@gmail.com>
2024-02-28 15:47:23 -0800

    Impl basic desktop application

---
+++ src/OpenGLAvalonia/App.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using OpenGLAvalonia.ViewModels;
+using OpenGLAvalonia.Views;
+
+namespace OpenGLAvalonia;
+
+public partial class App : Application
+{
+    public static double DpiScale { get; private set; } = 0;
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+            desktop.MainWindow = new ShellView {
+                DataContext = new ShellViewModel()
+            };
+
+            DpiScale = desktop.MainWindow.DesktopScaling;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ src/OpenGLAvalonia/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Immutable;
+
+namespace OpenGLAvalonia.ViewModels;
+
+public partial class ShellViewModel : ObservableObject
+{
+
+}
