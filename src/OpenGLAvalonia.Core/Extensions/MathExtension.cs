b6ea3dff68104a7649bcaf367d950234bc39bca6
ArchLeaders <archleadership28@gmail.com>
2024-02-28 15:48:25 -0800

    Create Math extension

---
+++ src/OpenGLAvalonia.Core/Extensions/MathExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace OpenGLAvalonia.Core.Extensions;
+
+public static class MathExtension
+{
+    public static float ToRadians(this float value)
+    {
+        return float.Pi / 180 * value;
+    }
+ }
